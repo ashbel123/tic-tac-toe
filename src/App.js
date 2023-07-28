83ba0724746e72f6cae26377a95b9c90c38477eb
Ashbel Raj <ashbelraj@gmail.com>
2023-07-28 18:10:40 +0530

    now it works without too many renders as we use the arrow function.

--- src/App.js
+++ src/App.js
@@ -19,23 +19,23 @@ const Board = () => {
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} SquareClick={HandleClick(0)} />
-        <Square value={squares[1]} SquareClick={HandleClick(1)} />
-        <Square value={squares[2]} SquareClick={HandleClick(2)} />
+        <Square value={squares[0]} SquareClick={()=>HandleClick(0)} />
+        <Square value={squares[1]} SquareClick={()=>HandleClick(1)} />
+        <Square value={squares[2]} SquareClick={()=>HandleClick(2)} />
         
         
 
       </div>
       <div className="board-row">
-        <Square value={squares[3]} SquareClick={HandleClick(3)} />
-        <Square value={squares[4]} SquareClick={HandleClick(4)} />
-        <Square value={squares[5]} SquareClick={HandleClick(5)} /> 
+        <Square value={squares[3]} SquareClick={()=>HandleClick(3)} />
+        <Square value={squares[4]} SquareClick={()=>HandleClick(4)} />
+        <Square value={squares[5]} SquareClick={()=>HandleClick(5)} /> 
 
       </div>
       <div className="board-row">
-        <Square value={squares[6]} SquareClick={HandleClick(6)} />
-        <Square value={squares[7]} SquareClick={HandleClick(7)} />
-        <Square value={squares[8]} SquareClick={HandleClick(8)} />
+        <Square value={squares[6]} SquareClick={()=>HandleClick(6)} />
+        <Square value={squares[7]} SquareClick={()=>HandleClick(7)} />
+        <Square value={squares[8]} SquareClick={()=>HandleClick(8)} />
 
       </div>
 
