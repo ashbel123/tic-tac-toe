6d5a7c92418fdbac80cd46cbd85347289b060d85
Ashbel Raj <ashbelraj@gmail.com>
2023-07-28 17:56:28 +0530

    updated the button to x when any of the button is clicked.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
 import { useState } from "react";
-const Square = ({value}) => { 
+const Square = ({value,SquareClick}) => { 
   
   // function handleClick() {
   //   console.log("clicked !");
   // }
   return (
-    <button className="square" >{value}</button>
+    <button className="square" onClick={SquareClick}>{value}</button>
   );
 
 };
 const Board = () => { 
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const HandleClick = () => {
+    const nextSquares = squares.slice();
+    nextSquares[0] = "x";
+    setSquares(nextSquares);
+  }
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} />
-        <Square value={squares[1]} />
-        <Square value={squares[2]} />
+        <Square value={squares[0]} SquareClick={HandleClick} />
+        <Square value={squares[1]} SquareClick={HandleClick} />
+        <Square value={squares[2]} SquareClick={HandleClick} />
         
         
 
       </div>
       <div className="board-row">
-        <Square value={squares[3]} />
-        <Square value={squares[4]} />
-        <Square value={squares[5]} /> 
+        <Square value={squares[3]} SquareClick={HandleClick} />
+        <Square value={squares[4]} SquareClick={HandleClick} />
+        <Square value={squares[5]} SquareClick={HandleClick} /> 
 
       </div>
       <div className="board-row">
-        <Square value={squares[6]} />
-        <Square value={squares[7]} />
-        <Square value={squares[8]} />
+        <Square value={squares[6]} SquareClick={HandleClick} />
+        <Square value={squares[7]} SquareClick={HandleClick} />
+        <Square value={squares[8]} SquareClick={HandleClick} />
 
       </div>
 
