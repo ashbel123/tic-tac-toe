245dd02dc790c750bfc0f47ddcd5517f8b543799
Ashbel Raj <ashbelraj@gmail.com>
2023-07-28 18:08:53 +0530

    committing with too many renders because we are placing '()' braces after the function which means to call the function while rendering the component itself so we should avoid it

--- src/App.js
+++ src/App.js
@@ -11,31 +11,31 @@ const Square = ({value,SquareClick}) => {
 };
 const Board = () => { 
   const [squares, setSquares] = useState(Array(9).fill(null));
-  const HandleClick = () => {
+  const HandleClick = (i) => {
     const nextSquares = squares.slice();
-    nextSquares[0] = "x";
+    nextSquares[i] = "x";
     setSquares(nextSquares);
   }
   return (
     <>
       <div className="board-row">
-        <Square value={squares[0]} SquareClick={HandleClick} />
-        <Square value={squares[1]} SquareClick={HandleClick} />
-        <Square value={squares[2]} SquareClick={HandleClick} />
+        <Square value={squares[0]} SquareClick={HandleClick(0)} />
+        <Square value={squares[1]} SquareClick={HandleClick(1)} />
+        <Square value={squares[2]} SquareClick={HandleClick(2)} />
         
         
 
       </div>
       <div className="board-row">
-        <Square value={squares[3]} SquareClick={HandleClick} />
-        <Square value={squares[4]} SquareClick={HandleClick} />
-        <Square value={squares[5]} SquareClick={HandleClick} /> 
+        <Square value={squares[3]} SquareClick={HandleClick(3)} />
+        <Square value={squares[4]} SquareClick={HandleClick(4)} />
+        <Square value={squares[5]} SquareClick={HandleClick(5)} /> 
 
       </div>
       <div className="board-row">
-        <Square value={squares[6]} SquareClick={HandleClick} />
-        <Square value={squares[7]} SquareClick={HandleClick} />
-        <Square value={squares[8]} SquareClick={HandleClick} />
+        <Square value={squares[6]} SquareClick={HandleClick(6)} />
+        <Square value={squares[7]} SquareClick={HandleClick(7)} />
+        <Square value={squares[8]} SquareClick={HandleClick(8)} />
 
       </div>
 
